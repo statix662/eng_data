e223552ac0ad374ab0059d4293e4cbcc556cff6f
Steve Yarnold <syarnold662@gmail.com>
2025-07-09 10:43:10 +0000

    add func for listing data dirs

--- src/eng_data/eng_data.py
+++ src/eng_data/eng_data.py
@@ -2,11 +2,15 @@ from importlib import resources
 
 # Get package data
 
-PACKAGE_DATA = resources.files("data")
+PACKAGE_DATA = resources.files("eng_data.data")
 
 # Get user data
 
 
-
 # Combine data souces
-DATA = [PACKAGE_DATA]
+DATA_DIRS = [PACKAGE_DATA]
+
+def get_dirs():
+    subdirs = [d for d in PACKAGE_DATA.iterdir() if d.is_dir() and d.name != "__pycache__"]
+    for d in subdirs:
+        print(d)
